60c1d42e23f636d4ea2cc77690a2995b55f60eb0
DGhambari <darrenghambari@yahoo.com>
2023-11-20 20:35:57 +0000

    Initial commit of template

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BurnV3"
+include(":app")
+ 
