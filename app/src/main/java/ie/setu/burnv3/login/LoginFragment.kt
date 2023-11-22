42874196763791435fe3891c2243d42c75259acb
DGhambari <darrenghambari@yahoo.com>
2023-11-22 21:31:47 +0000

    Login Screen created and users can log in to pre-created accounts in Firebase. Register and Forgot Password buttons are placeholders for now. A theme has been imported and can be toggled between light and dark mode in the code by alternating val colors = if (useDarkTheme) with val colors = if (!useDarkTheme) in line 81 of Theme.kt

---
+++ app/src/main/java/ie/setu/burnv3/login/LoginFragment.kt
@@ -0,0 +1,117 @@
+package ie.setu.burnv3.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import android.content.Context
+import android.widget.TextView
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun LoginScreen(onLoginSuccess: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        // Todo: Removed the title now until I can get the fonts configured correctly.
+//        Text("BURN", fontSize = 50.sp)
+//        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            modifier = Modifier.clip(shape = RoundedCornerShape(20.dp)),
+//            trailingIcon = {
+//                Icon(Icons.Filled.Clear, "")
+//            },
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            modifier = Modifier.clip(shape = RoundedCornerShape(20.dp)),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        // Todo: Write the function for the Forgot Password button.
+        TextButton(onClick = { }) {
+            Text("Forgot Password")
+        }
+        FilledTonalButton(onClick = { performLogin(email, password, context, onLoginSuccess) }) {
+            Text("Login")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Todo: Write the function for the Register button.
+        FilledTonalButton(onClick = { }) {
+            Text("Register")
+        }
+    }
+}
+
+private fun performLogin(
+    email: String,
+    password: String,
+    context: Context,
+    onLoginSuccess: () -> Unit
+) {
+    if (email.isEmpty() || password.isEmpty()) {
+        Toast.makeText(context, "Email and password must not be empty", Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    val auth = Firebase.auth
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onLoginSuccess()
+            } else {
+                val errorMessage = task.exception?.message ?: "Login failed"
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+}
